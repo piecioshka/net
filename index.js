98f87db55d0dea66731062247171a234417f5aa0
Bartłomiej Sobczuk <bartlomiej.sobczuk@gmail.com>
2018-01-13 23:13:03 +0100

    Display network connection status

--- index.js
+++ index.js
@@ -1,2 +1,67 @@
-#!/usr/bin/env node
+'use strict';
 
+(function () {
+    let $board = null;
+    
+    function clear() {
+        $board.innerHTML = '';
+    }
+    
+    function display(text) {
+        $board.innerHTML += `${text}\n`;
+    }
+    
+    function buildStatus() {
+        let connectionType = 'not supported';
+        let downlink = 'not supported';
+        let downlinkMax = 'not supported';
+        let effectiveType = 'not supported';
+        let rtt = 'not supported';
+        
+        // console.log(navigator.connection);
+        
+        if ('connection' in navigator) {
+            if ('type' in navigator.connection) {
+                connectionType = navigator.connection.type;
+            }
+            
+            if ('downlink' in navigator.connection) {
+                downlink = `${navigator.connection.downlink}Mbps`;
+            }
+            
+            if ('downlinkMax' in navigator.connection) {
+                downlinkMax = `${navigator.connection.downlinkMax}Mbps`;
+            }
+            
+            if ('effectiveType' in navigator.connection) {
+                effectiveType = navigator.connection.effectiveType;
+            }
+            
+            if ('rtt' in navigator.connection) {
+                rtt = navigator.connection.rtt;
+            }
+        }
+        
+        return { connectionType, downlink, downlinkMax, effectiveType, rtt };
+    }
+    
+    function logConnectionType() {
+        const { connectionType, downlink, downlinkMax, effectiveType, rtt } = buildStatus();
+        
+        clear();
+        display('net status:');
+        display(` - connection type: ${connectionType}`);
+        display(` - downlink: ${downlink}`);
+        display(` - downlinkMax: ${downlinkMax}`);
+        display(` - effectiveType: ${effectiveType}`);
+        display(` - rtt: ${rtt}`);
+    }
+    
+    function setup() {
+        $board = document.querySelector('pre');
+        logConnectionType();
+        window.navigator.connection.addEventListener('change', logConnectionType);
+    }
+    
+    setup();
+}());
